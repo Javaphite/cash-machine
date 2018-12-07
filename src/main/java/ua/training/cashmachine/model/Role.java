ce5e41cdd998d407cfba5afeac7247ca21a0dc9e
javaphite <javaphite@gmail.com>
2018-12-07 21:51:02 +0200

    Mapped paths/request commands accesses to user roles.

--- src/main/java/ua/training/cashmachine/model/Role.java
+++ src/main/java/ua/training/cashmachine/model/Role.java
@@ -1,31 +1,36 @@
 package ua.training.cashmachine.model;
 
 import ua.training.cashmachine.controller.Activity;
+import ua.training.cashmachine.model.utils.RoleUtils;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static ua.training.cashmachine.controller.Activity.*;
 
 public enum Role {
-    UNKNOWN_USER(Activity.TEST_ACTIVITY),
-    CASHIER(),
-    SENIOR_CASHIER(),
-    MERCHANDISER(),
-    MANAGER();
+    UNKNOWN_USER(LOGIN_MENU, LOGIN, CHANGE_LOCALE),
+    CASHIER(MAIN_MENU, INVOICE_MENU, LOGOUT, CHANGE_LOCALE_ALT, ADD_PRODUCT, CLOSE_INVOICE),
+    SENIOR_CASHIER(Role.CASHIER, REPORTS_MENU, JOURNAL_MENU, REMOVE_PRODUCT, CANCEL_INVOICE,
+            OPEN_TURN, CLOSE_TURN, MAKE_REPORT, REFUND, SHOW_INVOICE),
+    MERCHANDISER(MAIN_MENU, SUPPLIES_MENU, LOGOUT, CHANGE_LOCALE_ALT, GET_SUPPLIES, UPDATE_SUPPLIES),
+    MANAGER(Role.CASHIER, Role.SENIOR_CASHIER, Role.MERCHANDISER);
 
     private final List<String> pathsAllowed;
     private final List<String> commandsAllowed;
 
     Role(Activity... activitiesAllowed) {
-        pathsAllowed = Arrays.stream(activitiesAllowed)
-                .map(Activity::getPath)
-                .distinct()
-                .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+        pathsAllowed = RoleUtils.getAllowedPaths(activitiesAllowed);
+        commandsAllowed = RoleUtils.getAllowedCommands(activitiesAllowed);
+    }
 
-        commandsAllowed = Arrays.stream(activitiesAllowed)
-                .map(Activity::getMapping)
-                .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+    Role(Role... adoptedRoles) {
+        pathsAllowed = RoleUtils.getAllowedPaths(adoptedRoles);
+        commandsAllowed = RoleUtils.getAllowedCommands(adoptedRoles);
+    }
+
+    Role(Role adoptedRole, Activity... activitiesAllowed) {
+        pathsAllowed = RoleUtils.getAllowedPaths(adoptedRole, activitiesAllowed);
+        commandsAllowed = RoleUtils.getAllowedCommands(adoptedRole, activitiesAllowed);
     }
 
     public List<String> getCommandsAllowed() {
