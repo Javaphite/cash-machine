ae6f186315f9a3f3de5239feb45a69b52e04acd1
javaphite <javaphite@gmail.com>
2018-12-11 18:45:15 +0200

    AccessFilter updated from PathAccessFilter: add command access check and redirect filtering fixed.

---
+++ src/main/java/ua/training/cashmachine/controller/filter/AccessFilter.java
@@ -0,0 +1,45 @@
+package ua.training.cashmachine.controller.filter;
+
+import ua.training.cashmachine.controller.command.HttpServletCommand;
+import ua.training.cashmachine.controller.util.RequestInfoUtil;
+import ua.training.cashmachine.model.entity.Role;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/","/main", "/journal", "/invoice", "/reports", "/supplies"})
+public class AccessFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String actualPath = RequestInfoUtil.getActualPath(httpRequest);
+        String actualCommand = RequestInfoUtil.getActualCommand(httpRequest);
+        Role role = getActualUserRole(httpRequest);
+
+        if (role.getPathsAllowed().contains(actualPath)
+                && role.getCommandsAllowed().contains(actualCommand)) {
+            chain.doFilter(request, response);
+        } else {
+            //ToDo: complement with adequate message and custom exception
+            throw new RuntimeException("Access denied!");
+        }
+    }
+
+    private Role getActualUserRole(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Role role = (Role) session.getAttribute("role");
+        if(null == role) {
+            role = Role.UNKNOWN_USER;
+        }
+        return role;
+    }
+}
