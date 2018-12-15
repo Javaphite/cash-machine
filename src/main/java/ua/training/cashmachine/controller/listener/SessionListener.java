454eb5e359daee367be1beb37667f78a81fa7633
javaphite <javaphite@gmail.com>
2018-12-15 15:18:52 +0200

    Add user removal from active users on session destroy.

--- src/main/java/ua/training/cashmachine/controller/listener/SessionListener.java
+++ src/main/java/ua/training/cashmachine/controller/listener/SessionListener.java
@@ -1,11 +1,14 @@
 package ua.training.cashmachine.controller.listener;
 
+import com.mysql.cj.Session;
 import ua.training.cashmachine.model.service.UserService;
 
+import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.util.Collection;
 
 @WebListener
 public class SessionListener implements HttpSessionListener {
@@ -18,6 +21,14 @@ public class SessionListener implements HttpSessionListener {
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
+        HttpSession session = se.getSession();
+        ServletContext context = session.getServletContext();
+        String login = (String) session.getAttribute("login");
+        Collection<String> activeUsers = (Collection<String>) context.getAttribute("activeUsers");
 
+        if(activeUsers.contains(login)) {
+            activeUsers.remove(login);
+            session.getServletContext().setAttribute("users", activeUsers);
+        }
     }
 }
